83bc904721ddbd83b34f9a0841788710f47f5a21
Andersonxyx12 <167660355+Andersonxyx12@users.noreply.github.com>
2026-04-15 14:15:05 +0000

    conectar backend con mysql mediante variables de entorno

--- iac/api.tf
+++ iac/api.tf
@@ -6,4 +6,19 @@ resource "docker_container" "api" {
     internal = 3000
     external = var.api_port[terraform.workspace]
   }
+  
+  env = [
+    "NODE_ENV=${terraform.workspace}",
+    "DB_HOST=mysql-${terraform.workspace}-01",
+    "DB_PORT=3306",
+    "DB_USER=root",
+    "DB_PASSWORD=${var.mysql_root_password}",
+    "DB_NAME=appdb"
+  ]
+  
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+  
+  depends_on = [docker_container.mysql]
 }
